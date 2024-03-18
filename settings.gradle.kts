fa6539ab53a66b35b16c0c8202fc1a59c72e4a41
thedovla <donjakvlado@gmail.com>
2024-03-18 17:08:01 +0100

    Created project!

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "SE2Einzelphase"
+include(":app")
+ 
