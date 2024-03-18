4dad37dc60c382bf117c18efc139822a91e80947
thedovla <donjakvlado@gmail.com>
2024-03-18 18:23:56 +0100

    Declared variables in MainActivity

--- app/src/main/java/com/example/se2einzelphase/MainActivity.java
+++ app/src/main/java/com/example/se2einzelphase/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.se2einzelphase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView text;
+    private EditText MNr;
+    private TextView response;
+    private Button sendMNr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text = findViewById(R.id.textView1);
+        MNr = findViewById(R.id.inputText);
+        response = findViewById(R.id.textView2);
+        sendMNr = findViewById(R.id.button1);
     }
+
 }
